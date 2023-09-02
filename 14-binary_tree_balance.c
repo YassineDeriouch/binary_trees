3f8d6d35144bc403ef16fa8af6376c7849453b4d
YassineDeriouch <yderouich14@gmail.com>
2023-09-02 09:14:16 -0700

    14-binary_tree_balance.c fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,6 +7,7 @@
  *
  * Return: The height of the tree, or 0 if tree is NULL
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
     if (!tree)
@@ -25,6 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * Return: The balance factor of the tree, or 0 if tree is NULL
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
     if (!tree)
