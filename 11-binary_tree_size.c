363e6ab911b3240f8bc8ee27ac32e0c58a6f8692
YassineDeriouch <yderouich14@gmail.com>
2023-09-02 09:07:30 -0700

    11-binary_tree_size.c fix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,13 +7,16 @@
  *
  * Return: The size of the tree, or 0 if tree is NULL
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
     if (!tree)
         return 0;
+    {    
 
-    size_t left_size = binary_tree_size(tree->left);
-    size_t right_size = binary_tree_size(tree->right);
+	 size_t left_size = binary_tree_size(tree->left);
+	 size_t right_size = binary_tree_size(tree->right);
 
-    return left_size + right_size + 1;
+	 return left_size + right_size + 1;
+    }
 }
