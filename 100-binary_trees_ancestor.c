63f00238947d1902c69c384d5c8012af64556132
YassineDeriouch <yderouich14@gmail.com>
2023-09-03 09:36:33 -0700

    100-binary_trees_ancestor.c fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,25 +9,21 @@
  * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+                const binary_tree_t *second)
 {
+        binary_tree_t *n1, *n2;
 
-	binary_tree_t *n1, *n2;
+        if (!first || !second)
+                return (NULL);
+        if (first == second)
+                return ((binary_tree_t *)first);
 
-	if (first == NULL || second == NULL)
-		return (NULL);
-		
-	if (first == second)
-		return ((binary_tree_t *)first);
-
-	n1 = first->parent,
-	n2 = second->parent;
-
-	if (first == n2 || !n1 || (!n1->parent && n2))
-		return (binary_trees_ancestor(first, n2));
-
-	if (second == n1 || !n2 || (!n2->parent && n1))
-		return (binary_trees_ancestor(n1, second));
-
-	return (binary_trees_ancestor(n1, n2));
+        n1 = first->parent, n2 = second->parent;
+        if (first == n2 || !n1 || (!n1->parent && n2))
+                return (binary_trees_ancestor(first, n2));
+        else if (n1 == second || !n2 || (!n2->parent && n1))
+                return (binary_trees_ancestor(n1, second));
+        return (binary_trees_ancestor(n1, n2));
 }
+~
