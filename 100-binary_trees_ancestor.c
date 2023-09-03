a69b33548b460a268fd5946977f117d10077708e
YassineDeriouch <yderouich14@gmail.com>
2023-09-03 09:40:20 -0700

    19. Lowest common ancestor fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,22 +5,24 @@
  * @first: Pointer to the first node.
  * @second: Pointer to the second node.
  *
- * Return: If no common ancestors return NULL, else return common ancestor.
+ * Return: Pointer to the lowest common ancestor node, NULL if none.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	binary_tree_t *mom, *pop;
+	binary_tree_t *node1, *node2;
 
-	if (!first || !second)
+	if (first == NULL || second == NULL)
 		return (NULL);
 	if (first == second)
 		return ((binary_tree_t *)first);
 
-	mom = first->parent, pop = second->parent;
-	if (first == pop || !mom || (!mom->parent && pop))
-		return (binary_trees_ancestor(first, pop));
-	else if (mom == second || !pop || (!pop->parent && mom))
-		return (binary_trees_ancestor(mom, second));
-	return (binary_trees_ancestor(mom, pop));
+	node1 = first->parent, node2 = second->parent;
+
+	if (first == node2 || !node1 || (!node1->parent && node2))
+		return (binary_trees_ancestor(first, node2));
+	if (second == node1 || !node2 || (!node2->parent && node1))
+		return (binary_trees_ancestor(node1, second));
+
+	return (binary_trees_ancestor(node1, node2));
 }
